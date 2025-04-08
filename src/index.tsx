546a49999945bef5ca883e46ca76a66b511457ef
yimsarasorr <yimsara000@gmail.com>
2025-04-08 12:29:44 +0700

    wasm

--- src/index.tsx
+++ src/index.tsx
@@ -8,17 +8,27 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { ThemeWrapper } from "@mindee/web-elements.ui.theme-wrapper";
+import * as tf from "@tensorflow/tfjs";
+import "@tensorflow/tfjs-backend-wasm";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <ThemeWrapper style={{ padding: 0, color: "#001E3C" }}>
-      <App />
-    </ThemeWrapper>
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+async function initializeBackend() {
+  await tf.ready(); // Ensure TensorFlow.js is fully initialized
+  await tf.setBackend("wasm");
+  console.log(`[initializeBackend] TensorFlow.js backend set to: ${tf.getBackend()}`);
+}
+
+initializeBackend().then(() => {
+  ReactDOM.render(
+    <React.StrictMode>
+      <ThemeWrapper style={{ padding: 0, color: "#001E3C" }}>
+        <App />
+      </ThemeWrapper>
+    </React.StrictMode>,
+    document.getElementById("root")
+  );
+});
 
 // If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
+// to log results (for example: reportWebVitals.console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
