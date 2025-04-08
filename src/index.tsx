929ea44351f8829267ce2690a3dd44fb445c31d2
yimsarasorr <yimsara000@gmail.com>
2025-04-08 17:16:37 +0700

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -13,7 +13,7 @@ import "@tensorflow/tfjs-backend-wasm";
 
 async function initializeBackend() {
   await tf.ready(); // Ensure TensorFlow.js is fully initialized
-  await tf.setBackend("wasm");
+  await tf.setBackend("cpu");
   console.log(`[initializeBackend] TensorFlow.js backend set to: ${tf.getBackend()}`);
 }
 
