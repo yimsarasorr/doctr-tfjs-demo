4e31cbf446fe773c2a19f24d755d7c153a285dae
yimsarasorr <yimsara000@gmail.com>
2025-04-08 17:14:20 +0700

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -14,7 +14,7 @@ import { SpeedInsights } from "@vercel/speed-insights/react"
 
 async function initializeBackend() {
   await tf.ready(); // Ensure TensorFlow.js is fully initialized
-  await tf.setBackend("cpu");
+  await tf.setBackend("wasm");
   console.log(`[initializeBackend] TensorFlow.js backend set to: ${tf.getBackend()}`);
 }
 
